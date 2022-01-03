adfd8c285db84d0cd66aace36243fa31136950f9
DevRayHe <devrayhe@gmail.com>
2022-01-03 16:14:48 +1100

    userRoutes
    Updated user routes file with:
    
    - api/users  ( get all Users, create a single user )
    - api/users/:userId ( get single user by ID, update a user by ID, delete a user by ID)

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,3 +1,24 @@
 const router = require('express').Router();
 
+const {
+  getUsers,
+  getUsersbyId,
+  createUser,
+  updateUser,
+  deleteUser,
+} = require('../../controllers/userController');
+
+// api/users
+router
+  .route('/')
+  .get(getUsers)
+  .post(createUser);
+
+// api/users/:userId
+router
+  .route('/:userId')
+  .get(getUsersbyId)
+  .put(updateUser)
+  .delete(deleteUser);
+
 module.exports = router;
